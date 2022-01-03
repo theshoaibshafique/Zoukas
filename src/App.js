e4c9ca36aeaddd1ac48b6756f848efaa6fcc4af0
M.Shoaib Shafique <shoaibxp463@gmail.com>
2022-01-03 20:18:40 +0500

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import ProfilePage from "./pages/ProfilePage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/profile" element={<ProfilePage />} />
+      </Routes>
       {/* <LandingPage /> */}
-      <ProfilePage />
     </div>
   );
 }
